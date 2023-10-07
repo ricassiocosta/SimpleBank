d96b32e850dfa6e930fdc4a1cfff99385b6f2eb8
ricassiocosta <ricassiocosta.me@gmail.com>
2023-10-07 11:08:30 -0300

    fixing deadlock on transfertx

--- infra/db/sqlc/store.go
+++ infra/db/sqlc/store.go
@@ -50,11 +50,11 @@ type TransferTxParams struct {
 
 // TransferTxResult is the result of the transfer transaction
 type TransferTxResult struct {
-	Transfer    Transfer `json:"transfer`
-	FromAccount Account  `json:"from_account`
-	ToAccount   Account  `json:"to_account`
-	FromEntry   Entry    `json:"from_entry`
-	ToEntry     Entry    `json:"to_entry`
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
 }
 
 // TransferTx performs a money transaction from one account to another.
@@ -87,18 +87,21 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 			return err
 		}
 
-		result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-			ID:     arg.FromAccountID,
-			Amount: -arg.Amount,
-		})
+		parsedFromAccountID, err := uuid.Parse(arg.FromAccountID.String())
 		if err != nil {
 			return err
 		}
 
-		result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-			ID:     arg.ToAccountID,
-			Amount: arg.Amount,
-		})
+		parsedToAccountID, err := uuid.Parse(arg.ToAccountID.String())
+		if err != nil {
+			return err
+		}
+
+		if parsedFromAccountID.ClockSequence() < parsedToAccountID.ClockSequence() {
+			result.FromAccount, result.ToAccount, err = addMoney(ctx, q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
+		} else {
+			result.ToAccount, result.FromAccount, err = addMoney(ctx, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
+		}
 		if err != nil {
 			return err
 		}
@@ -107,4 +110,31 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 	})
 
 	return result, err
+}
+
+func addMoney(
+	ctx context.Context,
+	q *Queries,
+	accountID1 uuid.UUID,
+	accountAmount1 int64,
+	accountID2 uuid.UUID,
+	accountAmount2 int64,
+) (fromAccount Account, toAccount Account, err error) {
+	fromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     accountID1,
+		Amount: accountAmount1,
+	})
+	if err != nil {
+		return
+	}
+
+	toAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     accountID2,
+		Amount: accountAmount2,
+	})
+	if err != nil {
+		return
+	}
+
+	return
 }
